72106fbd4ea80252622781ad5bd08b16b4247348
Amylos <andrewgarnierpro@gmail.com>
2024-01-29 23:14:31 +0100

    version à présenter

--- ReactSymfony/src/Form/RegistrationFormType.php
+++ ReactSymfony/src/Form/RegistrationFormType.php
@@ -43,10 +43,18 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'User' => 'ROLE_USER',
+                ],
+                'label' => false,
+                'attr' => ['class' => 'input-style'],
+                'multiple' => true,
+                'mapped' => false,
+            ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
-                
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password', 'placeholder' => 'Password', 'class' => 'input-style'],
                 'constraints' => [
